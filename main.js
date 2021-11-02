d6c12a6dc1310f5578ae72ab66ca24e0f6b58a55
Kenneth David <lapidk3@yahoo.com>
2021-11-02 10:19:01 -0700

    finished the project

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-$(document).ready(function() {
-// write your code here
+const PLACES = "places";
 
+
+// Read the data from the JSON file 
+$(document).ready(function() {
+    $.getJSON('data.json', function(data) {
+        let theTable = document.getElementById(PLACES);
+        data.forEach(function(elements) {
+           var tbl_row = theTable.insertRow();
+           var button = document.createElement('BUTTON');
+           var text = document.createTextNode("Open in Google Maps");
+           button.appendChild(text);
+           var link = "https://www.google.com/maps?q=" + elements["location"];
+           button.onclick = function() {
+                location.href = link;
+           };
+           tbl_row.insertCell().innerHTML = elements["name"];
+           tbl_row.insertCell().innerHTML = elements["description"];
+           tbl_row.insertCell().appendChild(button);
+        });
+    });
 });
